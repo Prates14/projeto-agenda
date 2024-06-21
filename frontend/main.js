84fd08243724f07ed8c8962e4a37f59a5520865b
prates <guipratesdsantos@gmail.com>
2024-06-21 16:50:41 -0300

    projeto agenda separando submit de login e cadastro

--- frontend/main.js
+++ frontend/main.js
@@ -9,15 +9,18 @@ document.getElementById('trocar').addEventListener('click', function () {
     const formTitle = document.getElementById('form-title');
     const formButton = document.getElementById('form-button');
     const toggleFormText = document.getElementById('toggle-form');
+    const action = document.getElementById('toggle-action');
 
     // Verificar o estado atual e trocar os valores
     if (formTitle.innerText === 'Login') {
         formTitle.innerText = 'Cadastro';
         formButton.innerText = 'CADASTRAR';
         toggleFormText.innerText = 'entrar';
+        action.setAttribute('action', '/login/register');
     } else {
         formTitle.innerText = 'Login';
         formButton.innerText = 'ENTRAR';
         toggleFormText.innerText = 'cadastrar';
+        action.setAttribute('action', '/login/profile');
     }
 });
